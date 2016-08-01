14d2f824a334b8d7257557f638c4511f9fe09f21
prashant <prashantchikhalkar@gmail.com>
2016-08-01 12:27:24 +0530

    Update assignment6.py

--- Module3/assignment6.py
+++ Module3/assignment6.py
@@ -7,25 +7,30 @@ import matplotlib.pyplot as plt
 # It's located at 'Datasets/wheat.data'
 # 
 # .. your code here ..
-
+df = df.read_csv ('Datasets/wheat.data')
+print df.head ()
 
 #
 # TODO: Drop the 'id' feature
 # 
 # .. your code here ..
-
+df = df.drop (labels = ['id'], axis = 1)
 
 #
 # TODO: Compute the correlation matrix of your dataframe
 # 
 # .. your code here ..
-
+df.corr ()
 
 #
 # TODO: Graph the correlation matrix using imshow or matshow
 # 
 # .. your code here ..
-
+plt.imshow (df.corr (), cmap = plt.cm.Blues, inerpolation = 'nearest')
+plt.colorbar ()
+tick_marks = [i for i in range (len(df.columns))]
+plt.xticks (tack_marks, df.columns, rotation = 'vertical')
+plt.yticks (tack_marks, df.columns)
 
 plt.show()
 
