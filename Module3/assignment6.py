26e8306f5fb38bc5ab4dadd4c54a54f781acb83b
prashant <prashantchikhalkar@gmail.com>
2016-08-02 09:56:21 +0530

    Update assignment6.py

--- Module3/assignment6.py
+++ Module3/assignment6.py
@@ -21,17 +21,17 @@ wheat = wheat.drop (labels = ['id'], axis = 1)
 # TODO: Compute the correlation matrix of your dataframe
 # 
 # .. your code here ..
-df.corr ()
+wheat.corr ()
 
 #
 # TODO: Graph the correlation matrix using imshow or matshow
 # 
 # .. your code here ..
-plt.imshow (df.corr (), cmap = plt.cm.Blues, inerpolation = 'nearest')
+plt.imshow (wheat.corr (), cmap = plt.cm.Blues, inerpolation = 'nearest')
 plt.colorbar ()
-tick_marks = [i for i in range (len(df.columns))]
-plt.xticks (tack_marks, df.columns, rotation = 'vertical')
-plt.yticks (tack_marks, df.columns)
+tick_marks = [i for i in range (len(wheat.columns))]
+plt.xticks (tack_marks, wheat.columns, rotation = 'vertical')
+plt.yticks (tack_marks, wheat.columns)
 
 plt.show()
 
