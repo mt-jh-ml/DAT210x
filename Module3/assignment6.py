c3b9dc9f3d50caff73b49951d59e74f187e011e0
PRASHANT CHIKHALKAR <prashantchikhalkar@gmail.com>
2016-08-06 15:56:43 +0530

    Updated assignment4.py assignment5.py

--- Module3/assignment6.py
+++ Module3/assignment6.py
@@ -7,14 +7,13 @@ import matplotlib.pyplot as plt
 # It's located at 'Datasets/wheat.data'
 # 
 # .. your code here ..
-wheat = df.read_csv ('Datasets/wheat.data')
-print wheat.head ()
+wheat = pd.read_csv ('Datasets/wheat.data')
+#print wheat.head ()
 
 #
 # TODO: Drop the 'id' feature
 # 
 # .. your code here ..
-#df = df.drop (labels = ['id'], axis = 1)
 wheat = wheat.drop (labels = ['id'], axis = 1)
 
 #
@@ -27,12 +26,10 @@ wheat.corr ()
 # TODO: Graph the correlation matrix using imshow or matshow
 # 
 # .. your code here ..
-plt.imshow (wheat.corr (), cmap = plt.cm.Blues, inerpolation = 'nearest')
+plt.imshow (wheat.corr(), cmap=plt.cm.Blues, inerpolation='nearest')
 plt.colorbar ()
 tick_marks = [i for i in range (len(wheat.columns))]
-plt.xticks (tack_marks, wheat.columns, rotation = 'vertical')
-plt.yticks (tack_marks, wheat.columns)
+plt.xticks (tick_marks, wheat.columns, rotation='vertical')
+plt.yticks (tick_marks, wheat.columns)
 
 plt.show()
-
-
